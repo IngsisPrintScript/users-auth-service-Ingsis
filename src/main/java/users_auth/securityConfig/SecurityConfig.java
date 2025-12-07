e788994e000ae8b3eba67717702afc835095b585
Santino2005 <santinocolombo13@gmail.com>
2025-12-07 16:36:47 -0300

    Changed the cors

--- src/main/java/users_auth/securityConfig/SecurityConfig.java
+++ src/main/java/users_auth/securityConfig/SecurityConfig.java
@@ -34,8 +34,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
-                .csrf(AbstractHttpConfigurer::disable);
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults())).csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
